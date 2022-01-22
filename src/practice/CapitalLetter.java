31f7ac1812b4b148d23a097cf116ff8fda36b019
김준또 <@61.77.121.185>
2022-01-22 23:40:21 +0900

    P.93 7번

--- src/practice/CapitalLetter.java
+++ src/practice/CapitalLetter.java
@@ -1,5 +1,22 @@
 package practice;
+import java.util.Scanner;
 
 public class CapitalLetter {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("영어문장을 입력하시오.");
+		String engline = input.nextLine();
+		int place = engline.indexOf("Java"); //Java라고 시작하는 배열의 시작 인덱스를 찾음
+		int end = engline.length();
+		String Lowerline = engline.substring(0, place);
+		String Cutline = engline.substring(place, end);
+		String Capitalline = Cutline.toUpperCase();
+		String result = Lowerline+Capitalline;
+		
+		System.out.println("수정된 문장은");
+		System.out.println(result);
+	}
 
 }

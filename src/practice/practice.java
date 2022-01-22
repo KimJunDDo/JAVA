f0319cf4e82340a0d83c949265ebd0898428bff9
김준또 <@61.77.121.185>
2022-01-22 22:29:36 +0900

    This is commit message!

--- src/practice/practice.java
+++ src/practice/practice.java
@@ -3,8 +3,8 @@ package practice;
 public class practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello! This is git practice");
+		//Git hub practice
 	}
 
 }

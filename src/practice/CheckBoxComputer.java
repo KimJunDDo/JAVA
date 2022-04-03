1c9fcff53aa45850cbe88664a2de4152ba42b3f7
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-04-03 17:15:41 +0900

    P.402 4번

--- src/practice/CheckBoxComputer.java
+++ src/practice/CheckBoxComputer.java
@@ -1,6 +1,7 @@
 package practice;
 
 import java.awt.Container;
+import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 import javax.swing.JCheckBox;
@@ -10,9 +11,10 @@ import javax.swing.JLabel;
 public class CheckBoxComputer extends JFrame {
 	private JCheckBox CPU, Mainboard, HDD, RAM, Monitor;
 	private JLabel sumLabel;
+	int sum = 0;
 	
 	public CheckBoxComputer() {
-		setSize(200, 350);
+		setSize(200, 250);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		CPU = new JCheckBox("CPU             220000원");
@@ -24,36 +26,53 @@ public class CheckBoxComputer extends JFrame {
 		Container cPane = getContentPane();
 		setLayout(null);
 		cPane.add(CPU);
-		CPU.setBounds(10, 10, 300, 50);
+		CPU.setBounds(10, 10, 300, 15);
 		cPane.add(Mainboard);
-		Mainboard.setBounds(10, 60, 300, 50);
+		Mainboard.setBounds(10, 40, 300, 15);
 		cPane.add(HDD);
-		HDD.setBounds(10, 110, 300, 50);
+		HDD.setBounds(10, 70, 300, 15);
 		cPane.add(RAM);
-		RAM.setBounds(10, 160, 300, 50);
+		RAM.setBounds(10, 100, 300, 15);
 		cPane.add(Monitor);
-		Monitor.setBounds(10, 210, 300, 50);
+		Monitor.setBounds(10, 130, 300, 15);
 		
-		MyComputerListener listener = new MyComputerListener();
-		
+		CheckBoxListener listener = new CheckBoxListener();
+		CPU.addItemListener(listener);
+		Mainboard.addItemListener(listener);
+		HDD.addItemListener(listener);
+		RAM.addItemListener(listener);
+		Monitor.addItemListener(listener);
 		
 		sumLabel = new JLabel("합계 = 0원");
 		sumLabel.setFont(sumLabel.getFont().deriveFont(20.0f));
 		cPane.add(sumLabel);
-		sumLabel.setBounds(10, 260, 300, 50);
+		sumLabel.setBounds(10, 150, 300, 50);
 		setVisible(true);
 	}
 	
-	@Override
-	class MyCoumputerListener implements ItemListener {
-		private int sum = 0;
-		int CCheck = CPU.isSelected() ? 1 : 0;		
-		int MainCheck = Mainboard.isSelected() ? 1 : 0;		
-		int HCheck = HDD.isSelected() ? 1 : 0;		
-		int RCheck = RAM.isSelected() ? 1 : 0;		
-		int MCheck = Monitor.isSelected() ? 1 : 0;		
+	private class CheckBoxListener implements ItemListener {
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			int selected = 1;
+			String item = ((JCheckBox)e.getItem()).getText();
+			if (e.getStateChange() == ItemEvent.SELECTED)
+				selected = 1;
+			else
+				selected = -1;
+			if (e.getItem() == CPU)
+				sum = sum + selected*220000;
+			else if (e.getItem() == Mainboard)
+				sum = sum + selected*180000;
+			else if (e.getItem() == HDD)
+				sum = sum + selected*80000;
+			else if (e.getItem() == RAM)
+				sum = sum + selected*60000;
+			else
+				sum = sum + selected*160000;
 			
-}
+			sumLabel.setText("합계 ="+sum+"원");	
+		}
+	}
 	
 	public static void main(String[] args) {
 		new CheckBoxComputer();
